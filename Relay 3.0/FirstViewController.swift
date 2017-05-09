e53099584741d111be9767bbef2ef099ef768827
Chan Hong <qing@Chans-iMac.local>
2017-05-09 16:06:22 +0800

    Initial Commit

---
+++ Relay 3.0/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Relay 3.0
+//
+//  Created by Chan Qing Hong on 9/5/17.
+//  Copyright © 2017 Chan Qing Hong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Relay 3.0/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Relay 3.0
+//
+//  Created by Chan Qing Hong on 9/5/17.
+//  Copyright © 2017 Chan Qing Hong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
